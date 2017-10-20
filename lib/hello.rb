c5dad8cae7976c9c4b4fb6afe28f81ad4f88b148
Jacqueline Lee <anginblu@gmail.com>
2017-10-20 03:21:43 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,5 @@
 def hello_t(array)
-  puts "#{name}"
-  yield
-  puts name.upcase
-  else
-    array.each do |name|
-      puts "#{name}"
-    end
-  end
+  yield(array)
 end
 
 # call your method here!
